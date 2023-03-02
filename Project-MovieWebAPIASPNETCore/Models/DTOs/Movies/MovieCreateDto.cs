4af1de3c1f0732a4adecedfeb0d8e9e575ed8203
Tommy Pham <60743602+tommy-ph@users.noreply.github.com>
2023-03-02 17:54:01 +0100

    Feat: Movie DTO class and profile and update MovieController

---
+++ Project-MovieWebAPIASPNETCore/Models/DTOs/Movies/MovieCreateDto.cs
@@ -0,0 +1,20 @@
+﻿using Project_MovieWebAPIASPNETCore.Models.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_MovieWebAPIASPNETCore.Models.DTOs.Movies
+{
+    public class MovieCreateDto 
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public string Director { get; set; }
+        public string Picture { get; set; }
+        public string Trailer { get; set; }
+
+        public int FranchiseId { get; set; }
+      
+
+    }
+}
---
+++ Project-MovieWebAPIASPNETCore/Profiles/MovieProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Project_MovieWebAPIASPNETCore.Models.Domain;
+using Project_MovieWebAPIASPNETCore.Models.DTOs.Movies;
+
+namespace Project_MovieWebAPIASPNETCore.Profiles
+{
+    public class MovieProfile: Profile
+    {
+        public MovieProfile()
+        {
+            CreateMap<Movie, MovieReadDto>()
+                .ForMember(dto => dto.Franchise, options => options.MapFrom(movieDomain => movieDomain.FranchiseId))
+                .ForMember(dto => dto.Characters, options =>options
+                .MapFrom(movieDomain => movieDomain.Characters
+                .Select(character => $"{character.CharacterId}").ToList()));
+
+            CreateMap<Movie, MovieEditDto>()
+                .ForMember(dto => dto.FranchiseId, options => options
+                .MapFrom(movieDomain => movieDomain.FranchiseId)).ReverseMap();
+
+
+            CreateMap<Movie, MovieCreateDto>()
+                .ForMember(dto => dto.FranchiseId, options => options
+                .MapFrom(movieDomain => movieDomain.FranchiseId))
+                .ReverseMap();
+        }
+    }
+}
